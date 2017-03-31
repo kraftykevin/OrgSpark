164587c491f70c26cf90fd42078b4b48b5857854
kevin_kraft <orgspark@gmail.com>
2017-03-31 14:20:14 -0700

    signup, login

--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, name='logout'), #bug here, need fix.
 ]
